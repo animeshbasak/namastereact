755224ebc3b86f18e8f91a1757e96f6fa8ec3b3a
animeshbasak <animeshsbasak@gmail.com>
2023-01-07 02:31:07 +0530

    Lecture 2 commit

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
 const heading1 = React.createElement("h1", { id: "title" }, "Heading1");
 const heading2 = React.createElement("h2", { id: "title" }, "Heading2");
 // console.log(heading)
